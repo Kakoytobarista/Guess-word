797439f418c92630c9d31482ade5ba36431ea5ce
Aslan <HeyAslan@yandex.ru>
2022-07-18 01:49:18 +0200

    Add min value validator to model

--- backend/guess_word/guess_app/models.py
+++ backend/guess_word/guess_app/models.py
@@ -1,10 +1,13 @@
 import uuid as uuid
+
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Word(models.Model):
     word = models.CharField(verbose_name='word',
                             max_length=13,
+                            validators=[MinValueValidator],
                             unique=False)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
