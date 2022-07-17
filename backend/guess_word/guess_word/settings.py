e61f9d099d89e59cf9108ace00f75beb39ec54a3
root <root@fhm3k05duvp73qj7c0vl.auto.internal>
2022-07-17 23:02:33 +0000

    Add migrations

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -2,6 +2,9 @@ import os
 
 from corsheaders.defaults import default_headers
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
