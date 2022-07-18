56b3cce00e7a3a4180be20015aa82b06eeb0e210
Aslan <HeyAslan@yandex.ru>
2022-07-18 02:00:01 +0200

    Create custom validator

--- backend/guess_word/guess_app/models.py
+++ backend/guess_word/guess_app/models.py
@@ -2,12 +2,14 @@ import uuid as uuid
 
 from django.db import models
 
+from backend.guess_word.guess_app.validators import validate_length
+
 
 class Word(models.Model):
     word = models.CharField(verbose_name='word',
                             max_length=13,
-                            min_length=1,
-                            unique=False)
+                            unique=False,
+                            validators=[validate_length])
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     def __str__(self):
