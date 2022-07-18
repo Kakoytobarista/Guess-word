73a4f684aa136a72ad06616867547b40faee1d3f
Aslan <HeyAslan@yandex.ru>
2022-07-18 02:03:44 +0200

    change value

--- backend/guess_word/guess_app/validators.py
+++ backend/guess_word/guess_app/validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_length(value, length=2):
+def validate_length(value, length=1):
     """Validator for check length of string no less 2"""
     if len(str(value)) != length:
         raise ValidationError(u'%s is not the correct length' % value)
