4edeebeb1059c1d46bed8ad3045520722d561893
Aslan <HeyAslan@yandex.ru>
2022-07-18 15:58:39 +0200

    test sentry

--- backend/guess_word/guess_word/urls.py
+++ backend/guess_word/guess_word/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
