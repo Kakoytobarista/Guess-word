383ed1e02b54fc9292e830374fb3ed0352d38394
Aslan <HeyAslan@yandex.ru>
2022-07-19 15:36:09 +0200

    Add one test

--- backend/guess_word/api/tests.py
+++ backend/guess_word/api/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+from guess_app.models import Word
+
 # Create your tests here.
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.word = Word.objects.create(username='word')
+
+    def test_post_have_correct_object_name(self):
+        """Проверяем, что у модели Post строка возвращаемая
+         методом __str__ берется из поля text."""
+
+        self.assertIn(self.word.__str__(), self.word.text)
+
+    # def test_post_have_length_str_method_with_15_symbols(self):
+    #     """Проверяем, что у модели Post длина строки
+    #      возвращаемая методом __str__ не более 15 символов."""
+    #
+    #     self.assertLess(len(self.post.__str__()), 16)
