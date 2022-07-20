33d7b2013c7a71c25c61c57f407c91453ed71939
root <root@fhm3k05duvp73qj7c0vl.auto.internal>
2022-07-20 20:48:40 +0000

    delete comments from file

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -9,23 +9,16 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '51.250.90.3', 'guess-word.onthewifi.com']
 
 
-# Application definition
 
 sentry_sdk.init(
     dsn=os.getenv('SENTRY_KEY'),
