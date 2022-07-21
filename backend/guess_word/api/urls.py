37e9e24085edc3f39bc4ea5d87d935afa120d2d6
Aslan <HeyAslan@yandex.ru>
2022-07-21 19:59:23 +0200

    Add ratelimit technology

--- backend/guess_word/api/urls.py
+++ backend/guess_word/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
+from ratelimit.decorators import ratelimit
 
 from api.views import WordViewSet
 
 
 router = routers.DefaultRouter()
-router.register('word', WordViewSet, basename='word')
+router.register('word', ratelimit(key='ip', method='POST', rate='1/m')(WordViewSet, basename='word'))
 
 
 urlpatterns = (
