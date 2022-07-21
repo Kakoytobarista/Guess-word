460e5033e49a595650acc8274f602f6fe5814380
Aslan <HeyAslan@yandex.ru>
2022-07-21 20:50:53 +0200

    Add rate limit

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -121,7 +121,13 @@ if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing an
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '8/min',
+    }
 }
 
 
