f1b172e534ba60e8102038a9798f528100f9f2d2
root <root@fhm3k05duvp73qj7c0vl.auto.internal>
2022-07-21 23:11:29 +0000

    add imports

--- backend/guess_word/api/views.py
+++ backend/guess_word/api/views.py
@@ -5,6 +5,9 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 from guess_app.models import Word
 from api.serializers import WordSerializers
