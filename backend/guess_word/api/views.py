390f0a0e477d03e299be46fe2edace16b08fbeff
Aslan <HeyAslan@yandex.ru>
2022-07-21 22:27:11 +0200

    Configurate redis

--- backend/guess_word/api/views.py
+++ backend/guess_word/api/views.py
@@ -1,10 +1,13 @@
 import random
 
 from django_filters.rest_framework import DjangoFilterBackend
-from ratelimit.decorators import ratelimit
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 
 from guess_app.models import Word
@@ -17,6 +20,13 @@ class WordViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, ]
     filterset_fields = ['word', 'uuid']
 
+    # @method_decorator(vary_on_cookie)
+    # @method_decorator(cache_page(10))
+    # def dispatch(self, *args, **kwargs):
+    #         return super(WordViewSet, self).dispatch(*args, **kwargs)
+
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(10))
     @action(methods=['get'],
             detail=False, )
     def random_word(self, *args):
