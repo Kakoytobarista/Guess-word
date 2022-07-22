e754d6dd82b3aab4056513a69c267cb0f626cee0
KakoytoBarista <baristan1@rambler.ru>
2022-07-22 13:06:34 +0000

    Add cache to random word

--- backend/guess_word/api/views.py
+++ backend/guess_word/api/views.py
@@ -5,10 +5,10 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-<<<<<<< HEAD
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
-=======
->>>>>>> 97338d7580a817edf9926f6288dc2e9264993d78
 from guess_app.models import Word
 from api.serializers import WordSerializers
 
@@ -19,6 +19,9 @@ class WordViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, ]
     filterset_fields = ['word', 'uuid']
 
+
+    @method_decorator(vary_on_cookie)
+    @method_decorator(cache_page(8*1))
     @action(methods=['get'],
             detail=False, )
     def random_word(self, *args):
