a4809b2d80db65d31fc8d8d277df3fc4d6eabade
aslan <baristan1aslan@gmail.com>
2022-10-19 17:35:05 +0200

    Add domen to cors

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -63,8 +63,8 @@ CORS_ORIGIN_ALLOW_ALL=True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'http://guess-word.onthewifi.com',
-    'http://51.250.94.71',
+    'http://aslanrdl.ddns.net',
+    'http://143.110.153.168',
 ]
 
 
