e95bde87d8c279281f2c84b9976a2f1ab341d725
aslan <baristan1aslan@gmail.com>
2022-12-15 18:48:03 +0100

    Replace middleware

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware'
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware'
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
