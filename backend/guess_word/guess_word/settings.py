d946d6d9f3a741ec4efce01fce42e520f5b9f291
aslan <baristan1aslan@gmail.com>
2022-12-15 19:31:07 +0100

    Delete wrong middleware

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -52,8 +52,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
