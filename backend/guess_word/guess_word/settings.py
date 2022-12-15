d222d799ef62f0fead6b3e8e5d8ce91e6fcedea0
aslan <baristan1aslan@gmail.com>
2022-12-15 19:35:34 +0100

    Delete middleware

--- backend/guess_word/guess_word/settings.py
+++ backend/guess_word/guess_word/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'rest_framework.authentication.BasicAuthentication',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
